49e95c6286adb40831b2ad6125ec35dd70b3b84d
apbhat <apbhat@users.noreply.github.com>
2015-08-19 10:45:15 +0530

    Update fan.cpp

--- fan.cpp
+++ fan.cpp
@@ -1,2 +1,5 @@
-cout<<"Hi! I'm Apoorva.\n I'm here to introduce you to the world of Fangirling.";
+cout<<"Hi! I'm Apoorva.";
+cout<<"I'm here to introduce you to the world of Fangirling.";
 cout<<"Fangirling is basically obsessing over something or someone.";
+cout<<"hm";
+cout<<"This may include a band, a tv show, a movie or any thing in that pattern";
